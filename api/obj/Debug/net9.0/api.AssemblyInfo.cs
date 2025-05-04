aae5e118f1390bae5e71568dea561ea579dc3c9e
BrayRpgs <brayanymelquisedec@gmail.com>
2025-05-03 20:31:32 -0600

    -add structure folders

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5cb77bea453cf75c69407b149c3273cc7b5651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82bd0c5f006ef8dc31483d4cffd0361f229549f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
