8dae17541867ecd5edb05c61ff603f25f68766ca
BrayRpgs <brayanymelquisedec@gmail.com>
2025-05-16 00:31:06 -0600

    -- fix minors and optimize! -- date : 16-05-2025

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d2bdf9d30b1e5de55102c07f5784582bce3198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa3baf20aab19ee505a96b96dc5c818d09b096f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
